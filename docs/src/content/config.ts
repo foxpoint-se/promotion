18f9983246ccfd6919c8bf92b04512c3e5a484e1
bulingen <adamlecorney@gmail.com>
2023-08-26 17:02:23 +0200

    feat: dynamic news updates pages

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -9,8 +9,25 @@ const docsCollection = defineCollection({
     tags: z.optional(z.array(z.string())),
   }),
 });
+
+const updatesCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    image: z.optional(
+      z.object({
+        url: z.string(),
+        description: z.string(),
+      })
+    ),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   docs: docsCollection,
+  updates: updatesCollection,
 };
