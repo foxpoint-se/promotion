066b8fe1ec1d0d3aff15e9f223f61b1e452bf250
bulingen <adamlecorney@gmail.com>
2023-08-27 16:02:40 +0200

    fix: slightly prettier lists

--- docs/src/components/NewsUpdateItem.tsx
+++ docs/src/components/NewsUpdateItem.tsx
@@ -20,16 +20,15 @@ export const NewsUpdateItem = ({ update }: { update: Update }) => {
         </figure>
       )}
       <div className="card-body">
-        <h3 className="card-title">{update.title}</h3>
+        <h3 className="card-title">
+          <a href={update.linkUrl} className="link link-primary">
+            {update.title}
+          </a>
+        </h3>
         <div className="badge badge-ghost">
           {uglySimpleDateFormatting(update.date)}
         </div>
         <p>{update.description}</p>
-        <div className="card-actions justify-end">
-          <a href={update.linkUrl} className="btn btn-ghost">
-            Go to article
-          </a>
-        </div>
       </div>
     </div>
   );
